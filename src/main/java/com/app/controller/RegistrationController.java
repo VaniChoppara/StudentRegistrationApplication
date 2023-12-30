a8ce8d4cfb3e80319639d07f3b5c23ca010e1367
Sandya Choppara <sandyachoppara@gmail.com>
2023-12-30 16:24:53 +0800

    courses fetched using stored procedure

--- src/main/java/com/app/controller/RegistrationController.java
+++ src/main/java/com/app/controller/RegistrationController.java
@@ -11,11 +11,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.app.entity.StudentRegistration;
 import com.app.service.StudentRegistrationService;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @Controller
@@ -28,6 +28,7 @@ public class RegistrationController {
 	List<String> timings;
 	 
 	 @ModelAttribute
+	 @Transactional
 	 public void preLoad() {
 	  courses = studentRegistrationService.getCoursesList();
 	  timings=studentRegistrationService.getTimingsList();
--- src/main/java/com/app/repository/StudentRegistrationRepo.java
+++ src/main/java/com/app/repository/StudentRegistrationRepo.java
@@ -4,12 +4,25 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 
 import com.app.entity.StudentRegistration;
 
 public interface StudentRegistrationRepo extends JpaRepository<StudentRegistration, Integer> {
 
-	@Query(value="select course from courses", nativeQuery= true)
+//	@Query(value="select course from courses", nativeQuery= true)
+//	public List<String> getCourses();
+	
+// Procedure executed to get the course list
+	
+//	DELIMITER //
+//	Create PROCEDURE COURSE()
+//	BEGIN
+//	    select course from courses;
+//	END //    
+//	DELIMITER ;
+	
+	@Procedure("COURSE")
 	public List<String> getCourses();
 	
 	@Query(value="select timing from timings", nativeQuery= true)
